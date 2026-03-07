daeaacfb6e9ef843e39cafed1600575aa9595dc0
LeoVergaraS <leo.vergara@usach.cl>
2026-03-07 15:56:35 -0300

    add: components y assets

---
+++ app/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="container-header">
+        <img src="~/assets/logo_arvia.svg" alt="logo" width="128"/>
+    </header>
+</template>
+
+<style scoped>  
+    .container-header {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 1rem;
+        background-color: red;
+    }
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Esto es un header</h1>
+    <Header/>
     <slot/>
     <h1>Esto es un footer</h1>
 </template>
