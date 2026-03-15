f0243e16ac0125ed62fda9c407470cb917c31afa
LeoVergaraS <leo.vergara@usach.cl>
2026-03-15 03:49:06 -0300

    add: primeros estilos del header

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -5,20 +5,24 @@
 <template>
     <header class="flex items-center justify-between p-4">
         <img class="basis-0 grow" src="~/assets/svg/logo_arvia.svg" alt="logo" width="24"/>
-        <div class="basis-0 grow">
-            <p class="text-sm">Categorias</p>
+        <div class="basis-0 grow flex items-center justify-end gap-2">
+            <span class="text-primary-green">Categorias</span>
+            <Icon name="mdi:menu" size="1.5em" class="text-gold"/>
         </div>
         <div class="basis-0 grow-[3] flex flex-col items-center">
             <div>
-                <input class="w-100" type="text">
+                <input class="bg-gold rounded-full w-[500px] h-8 border-2 border-solid border-slate-50 shadow-lg" type="text">
+                <!--Falta el span-->
             </div>
-            <div>
-                <span class="px-3" v-for="value in tops" :key="value">{{ value }}</span>
-            </div>
+            <ul class="list-disc flex justify-between items-center gap-5 text-bold text-primary-green">
+                <li v-for="value in tops" :key="value">{{ value }}</li>
+            </ul>
         </div>
-        <div class="basis-0 grow">Entrar</div>
-        <div class="basis-0 grow flex items-center justify-end">
-            <p>Carro</p>
+        <div class="basis-0 grow flex items-center justify-start">
+            <button type="button" class="rounded-full bg-primary-green px-3 py-1 text-slate-50 font-bold border-4 border-solid border-gold ">Entrar</button>
+        </div>
+        <div class="basis-0 grow flex items-center px-2 justify-end">
+            <Icon name="mdi:shopping" size="2em" class="text-primary-green"/>
         </div>
     </header>
 </template>
